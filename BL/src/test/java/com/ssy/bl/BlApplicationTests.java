a14947d1267d84ad43cdcf6ec65727c36949fe0a
ssy <ssy@DESKTOP-1OFJGBT>
2022-03-03 23:04:47 +0900

    TEST

---
+++ BL/src/test/java/com/ssy/bl/BlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssy.bl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
